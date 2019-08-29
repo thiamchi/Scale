58e7c7691b1cbd5ba52f5739f1030957b1b75b5e
thiamchi <35895671+thiamchi@users.noreply.github.com>
2019-08-29 23:16:22 +0800

    Add project files.

---
+++ Scale.Wpf/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scale.Wpf
+{
+    public class Class1
+    {
+    }
+}
